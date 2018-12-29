5b605c479beb9578882fe9151c344123209498f6
WintaniaDev <46215571+WintaniaDev@users.noreply.github.com>
2018-12-29 14:51:41 +0700

    update another page and add new function

---
+++ src/js/Home.js
@@ -0,0 +1,25 @@
+document.getElementById("M1").onclick = function () {
+   location.href = "Countdown.html";
+};
+
+document.getElementById("M2").onclick = function () {
+   location.href = "Profile.html";
+};
+
+document.getElementById("M3").onclick = function () {
+   // location.href = "Profile.html";
+   alert("Can't open Page");
+};
+
+document.getElementById("M4").onclick = function () {
+   alert("Can't open Page");
+};
+
+document.getElementById("M5").onclick = function () {
+   alert("Can't open Page");
+};
+
+document.getElementById("M6").onclick = function () {
+   alert("Can't open Page");
+};
+
